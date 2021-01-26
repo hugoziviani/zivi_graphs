6c25eb0bb2cf42f763c63d53baf4d3da28bd31be
Hugo Ziviani <hugo_ziviani@hotmail.com>
2021-01-26 09:31:22 -0300

    starting separete the vertex info

--- tp1.py
+++ tp1.py
@@ -9,25 +9,43 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 FILE_PATH = './input.txt'
 MODE_READ = 'r'
 MODE_WRITE = 'w'
 
 
-vertex_quantity = 0
-links = 0
-directed = 0
-
-def read_input(matrix):
+def read_input_and_insert_graph():
     file1 = open(FILE_PATH, MODE_READ)
     count = 0
+    line = file1.readline()
+    info = line.split()
     
-    while True: 
+    ######## Information graph #########
+    vertex_quantity = int(info[0])
+    links = int(info[1])
+    directed = bool(info[2])
+    
+    # print("vertex_quantity:", vertex_quantity) 
+    # print("links:", type(links))
+    # print("directed:", type(directed))
+
+    ######## Information graph #########
+    
+    while count <= vertex_quantity+1:
         
         line = file1.readline() 
         if not line: 
             break
-        print("Line{}: {}".format(count, line.strip())) 
+        info = line.split()
+
+        source = int(info[0])
+        destiny = int(info[1])
+        weight = int(info[2])
+        print("source:", source)
+        print("destiny:", destiny)
+        print("weight:", weight)
+        print()
         count += 1
     
     file1.close() 
@@ -45,19 +63,21 @@ def plot_graph():
     plt.savefig("path_graph1.png")
     plt.show()
 
-#read_input()
-# networkx
-
-
 def create_matrix(rows, columns):
     vertex_empty = {
         "source":0,
-        "target":0,
+        "target":0, # sí inteiro, pois cada um sera um vertice
         "weight":0
     }
     matrix = np.full((rows,columns), vertex_empty, dtype=type(vertex_empty))
     return matrix
 
 
-m = create_matrix(3,3)
-print(m)
+def main():
+    
+    read_input_and_insert_graph()
+    print()
+
+
+if __name__ == "__main__":
+    main()
