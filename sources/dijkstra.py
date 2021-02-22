e5d4915f7d7291dee4a3f6d052d8e6b5fbb65da0
Hugo Ziviani <hugo_ziviani@hotmail.com>
2021-02-22 08:34:37 -0300

    gonna go!

--- sources/dijkstra.py
+++ sources/dijkstra.py
@@ -1,27 +1,26 @@
 '''
 Entrada: Grafo (V, E), e matriz de pesos D{Dij}, para todas as arestas {i,j}
-dt[1]<-0; //considerando o vertice 1 como inicial
-rot[i]<-0;
-para i de 2 até n faça:
-    dt[i] <- infinito; //muito pesado, qualquer coisa é melhor.
-    rot[i] <- 0;
+dt[0]<-0; //considerando o vertice 1 como inicial
+rot[0]<-0; // não tem antecessor
+para i de 2 até n faça: //para todos os vertices validos vamos inicializar
+    dt[i] <- infinito; //muito pesado, distancia nao calculada ainda
+    rot[i] <- 0; // antecessor, pai do vertice do tempo de exec.
 
 A <- V; // recebe todos os vertices, pois eles estao abertos
 F <- Null; // nenhum vertice foi fechado ainda.
 
-enquanto F != V, faça:
-    v <- elemento de A, tal que dt[v] é o mínimo dentre os elementos de A;
+enquanto F != V, faça: //enquanto o grupo de fechados for diferente dos vertices do grafo (tem que explorar todos)
+    v <- elemento de A, tal que dt[v] é o mínimo dentre os elementos de A; na 1a exec. será o 0, pois ele é o que tem menor dt[0]
+
     F <- F U {v}; //adiciona nos fechados
     A <- A - {v}; // tira ele dos abertos
     N <- N - F; //conjunto de vizinhos do vertice v, menos os já fechados
 
     para u pertencente a N faça:
-        // o q é melhor?
+        // o q é melhor? o peso atual + o peso até o proximo ou a menor distancia já calculada 
         se dt[v]+dvu < dt[u] entao:
-            dt[u] <- dt[v]+dvu;
-            rot[u] <-v;
-
-
+            dt[u] <- dt[v]+dvu; //se for, atualiza a soma de pesos para a menor distancia calculada
+            rot[u] <-v; // guarda o antecessor
 
 '''
 
@@ -94,13 +93,46 @@ def read_input_and_insert_graph(file_input_path):
     return adjacences_list_updated, vertex_quantity, links, directed
 
 
-def dijsktra(graph):
-    print(graph)
+def dijkstra(graph): #retorna a menor distancia de um dado nó para todos os outros possíveis.
 
+    weight_control = {}
+    current_distance = {}
+    actual_weight = {}
+    to_be_visited = list(graph.keys())
+    actual_vertex = 0
+    actual_weight[actual_vertex] = 0
 
+    
+    for vertice in graph.keys():    
+        current_distance[vertice] = float('inf') #inicia os vertices como infinito
+
+    current_distance[actual_vertex] = 0 #distancia até ele mesmo
+    to_be_visited.remove(actual_vertex) #remove ele dos nao visitados
+
+    while to_be_visited: #enquanto existir no a ser visitado
+        neighborood = graph[actual_vertex]
+        for neighbor, weight in neighborood: # para cada visinho do nó atual, calcular a distancia para o proximo e atualizar se necessario
+            current_weight = weight + actual_weight[actual_vertex]
+            distancia = current_distance.get(neighbor, float("inf"))
+            if distancia > current_weight: # se a distancia até o vizinho for maior que o que acabou de ser calculado, atualiza
+                current_distance[neighbor] = current_weight
+                weight_control[neighbor] = current_distance[neighbor]
+
+        if not weight_control: 
+            break    
+        next_vertex = min(weight_control.items())
+        actual_vertex = next_vertex[0]
+        actual_weight[actual_vertex] = next_vertex[1]
+        to_be_visited.remove(actual_vertex)
+        del weight_control[actual_vertex]
+
+    
+    print(current_distance)
+
+    
 def main(argv):
-    file_input_path = "../inputs/input_dijkstra.txt"
-    file_output_path = "../outputs/output_dijkstra.txt" 
+    file_input_path = "/home/hz/Documents/grafos/zivi_graphs/inputs/input_dijkstra.txt"
+    file_output_path = "/home/hz/Documents/grafos/zivi_graphs/outputs/output_dijkstra.txt" 
     # file_input_path = argv[0]
     # file_output_path = argv[1]
     with open(file_output_path, MODE_WRITE) as writer:
@@ -111,7 +143,7 @@ def main(argv):
             
         else:
             print(f"{vertex_quantity} {links} NAO DIRECIONADO")
-        dijsktra(adjacences_list)
+        dijkstra(adjacences_list)
         # show_list_and_weights(adjacences_list, vertex_quantity, writer)
 
 if __name__ == "__main__":
